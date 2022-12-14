446110a4c1ae554c002a4b0fa05b74f0752dab9a
vi-ht <74223356+vi-ht@users.noreply.github.com>
2022-12-15 00:43:19 +0700

    set position for background items

--- js/myJS.js
+++ js/myJS.js
@@ -19,12 +19,20 @@ function onLoad() {
 window.addEventListener("scroll", () => {
     var scrollY = this.scrollY;
     // document.getElementById("test").innerText = scrollY;
+    if(scrollY === 0){
+        document.getElementById("mobile-nav").style.backgroundColor = "rgba(0, 0, 0, 0)";
+        document.getElementById("mobile-nav").style.boxShadow = "none";
+    }
+    if(scrollY > 0){
+        document.getElementById("mobile-nav").style.background = "white";
+        document.getElementById("mobile-nav").style.boxShadow = "4px 4px 4px 0 rgba(37, 114, 81, 0.06), 4px 6px 20px 0 rgba(37, 114, 81, 0.06)";
+    }
     if(scrollY>250 && scrollY<900){
-        document.getElementById("plant-a-tree").style.right = 0;
+        document.getElementById("plant-a-tree").style.right = "-12px";
     }else if(scrollY>=900 && scrollY< 1750){
-        document.getElementById("live-sustainably").style.left = 0;
+        document.getElementById("live-sustainably").style.left = "12px";
     } else if(scrollY>=1750){
-        document.getElementById("save-energy").style.right = 0;
+        document.getElementById("save-energy").style.right = "-12px";
         
     }
     
